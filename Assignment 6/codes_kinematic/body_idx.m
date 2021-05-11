24fcb74acbe3426cac20f6fee5068b9e66ef03fe
RuslanMalygin <80762842+RuslanMalygin@users.noreply.github.com>
2021-05-12 00:34:12 +0300

    Add files via upload

---
+++ Assignment 6/codes_kinematic/body_idx.m
@@ -0,0 +1,5 @@
+
+function indices = body_idx(ii)
+%BODY_IDX Returns indices of the coordinates for body ii
+
+indices = 3 * ii + (-2:0);
---
+++ Assignment 6/codes_kinematic/rot.m
@@ -0,0 +1,7 @@
+
+function A = rot(phi)
+% Rotational transformation matrix for planar case
+cp = cos(phi);
+sp = sin(phi);
+A = [cp, -sp
+    sp, cp];
